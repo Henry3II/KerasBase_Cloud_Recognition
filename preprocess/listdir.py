c0610a68c3194fead943f403a6f6d2f2f536289c
Henry3II <916191052@qq.com>
2019-04-07 21:02:11 +0800

    Update listdir.py

--- preprocess/listdir.py
+++ preprocess/listdir.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-  
 import os  
   
-def listdir(path, list_name):  #传入存储的list
+def listdir(path, list_name):  
 	for file in os.listdir(path):  
 		file_path = os.path.join(path, file)  
 		if os.path.isdir(file_path):  
 			listdir(file_path, list_name)  
 		else:  
-			list_name.append(file_path[9:])    #将前面的路径去掉，只保留文件名
+			list_name.append(file_path[9:]) 
 	print(list_name)
 
 listdir('data_ext/',[])
